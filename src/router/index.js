8d465abb0b47eff66141601a1490c71982464686
倚栏听风 <1845706088@qq.com>
2020-07-18 12:35:08 +0800

    完成报表

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params'
 import GoodsList from '../components/goods/List'
 import Add from '../components/goods/Add'
 import Order from '../components/order/Order'
+import Report from '../components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -52,6 +53,9 @@ const routes = [{
     }, {
       path: '/orders',
       component: Order
+    }, {
+      path: '/reports',
+      component: Report
     }
   ]
 }]
